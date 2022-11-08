af123ae30081b473a424c6aefac8f0ad6a33ffc9
Hell-Tractor <1104840448@qq.com>
2022-11-08 17:38:30 +0800

    Add minnium spanning tree

---
+++ codes/kruskal.cpp
@@ -0,0 +1,18 @@
+int n, m;
+vector<tuple<int, int, int>> edge;
+int Kruskal() {
+  int ans = 0, count = 0;
+  sort(all(edge), [](const tuple<int, int, int>& a, const tuple<int, int, int>& b) {
+	return get<2>(a) < get<2>(b);
+  });
+  UFS ufs(edge.size() + 1);
+  for (auto& [u, v, w] : edge) {
+    if (!ufs.check(u, v)) {
+      ans += w;
+      count++;
+      if (count == n - 1)
+        return ans;
+      ufs.union(u, v);
+    }
+  }
+}
