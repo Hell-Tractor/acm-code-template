2ae8420f6f6c60f472f94b397d243d43eb29318d
Hell-Tractor <1104840448@qq.com>
2023-10-06 21:12:37 +0800

    添加数学公式

--- codes/hierholzer.cpp
+++ codes/hierholzer.cpp
@@ -1,17 +1,17 @@
-int cnt[N];
+int cnt[N];		// * 初始值为0
 vector<Edge> edge[N];
 vector<int> ans;
 void Hierholzer(int x) {
-  for (int& i = cnt[x]; i < (int)edge[x].size();) {
-	if (edge[x][i].exists) {	// 标记边是否走过
-	  edge e = edge[x][i];
-	  edge[x][i].exists = 0;
-	  edge[e.to][e.revref].exists = 0;	// 同时标记反向边
-	  ++i;
-	  Hierholzer(e.to);
-	} else {
-	  ++i;
+	for (int& i = cnt[x]; i < (int)edge[x].size();) {
+		if (edge[x][i].exists) {  // 标记边是否走过
+			edge e = edge[x][i];
+			edge[x][i].exists = 0;
+			edge[e.to][e.revref].exists = 0;  // 同时标记反向边
+			++i;
+			Hierholzer(e.to);
+		} else {
+			++i;
+		}
 	}
-  }
-  ans.push(x);
+	ans.push(x);
 }
